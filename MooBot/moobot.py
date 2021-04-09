62672a9627daccb983d99b471bb9f68ae9edf453
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2021-04-09 20:25:00 +0600

    Update moobot.py

--- MooBot/moobot.py
+++ MooBot/moobot.py
@@ -14,7 +14,7 @@ import os
 STUDENT_ID = "YOUR_STUDENT_ID"
 LOGIN_PASSWORD = "YOUR_LOGIN_PASSWORD"
 CHANNEL_ID = YOUR_CHANNEL_ID # don't use double quote
-GROUP_ID = "YOUR_FACEBOOK_GROUP_ID" # don't use double quote
+GROUP_ID = YOUR_FACEBOOK_GROUP_ID # don't use double quote
 
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
